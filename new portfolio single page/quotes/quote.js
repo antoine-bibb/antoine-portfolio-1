73ce35d85692c22b1db69613be62e3cab19f6412
Antoine Bibb <69204210+antoine-bibb@users.noreply.github.com>
2020-12-11 20:53:54 -0500

    Add files via upload

---
+++ new portfolio single page/quotes/quote.js
@@ -0,0 +1,63 @@
+const quoteContainer = document.getElementById("quote-container");
+const quoteText = document.getElementById("quote");
+const authorText = document.getElementById("author");
+const twitterBtn = document.getElementById("twitter");
+const newQuoteBtn = document.getElementById("new-quote");
+const loader = document.getElementById("loader");
+
+//show loading
+function loading() {
+  loader.hidden = false;
+  quoteContainer.hidden = true;
+}
+
+//hide loading
+function complete() {
+  if (!loader.hidden) {
+    quoteContainer.hidden = false;
+    loader.hidden = true;
+  }
+}
+//get Quote from api
+async function getQuote() {
+  loading();
+  const proxyUrl = "https://whispering-depths-47619.herokuapp.com/";
+  const apiUrl =
+    "http://api.forismatic.com/api/1.0/?method=getQuote&lang=en&format=json";
+  try {
+    const res = await fetch(proxyUrl + apiUrl);
+    const data = await res.json();
+    //if author is blank add unknown
+    if (data.quoteAuthor === "") {
+      authorText.innerText = "Unknown";
+    } else {
+      authorText.innerText = data.quoteAuthor;
+    }
+
+    //reduce font size for long quotes
+    if (data.quoteText.length > 120) {
+      quoteText.classList.add("long-quote");
+    } else {
+      quoteText.classList.remove("long-quote");
+    }
+    quoteText.innerText = data.quoteText;
+
+    //stop loader
+    complete();
+  } catch (err) {
+    getQuote();
+  }
+}
+//tweet quote
+function tweetQuote() {
+  const quote = quoteText.innerText;
+  const author = authorText.innerText;
+  const twitterUrl = `https://twitter.com/intent/tweet?text=${quote} - ${author}`;
+  window.open(twitterUrl, "_blank");
+}
+
+//event listeners
+newQuoteBtn.addEventListener("click", getQuote);
+twitterBtn.addEventListener("click", tweetQuote);
+// on load
+getQuote();
